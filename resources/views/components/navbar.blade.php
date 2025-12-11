65efa2d5fb5cf8f48928b323c647e2bbce56c3b2
unknown <rizkypratamalvbis@gmail.com>
2025-12-11 21:41:02 +0700

    send data to view

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,7 +9,7 @@
                       <div class="hidden md:block">
                           <div class="ml-10 flex items-baseline space-x-4">
                             <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-                            <x-nav-link href="/blog" :active="request()->is('blog')">Blog</x-nav-link>
+                            <x-nav-link href="/posts" :active="request()->is('posts')">Blog</x-nav-link>
                             <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
                             <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
                           </div>
@@ -59,15 +59,10 @@
 
           <div x-show="open" x-transition class="md:hidden">
               <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-                  <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                  <a href="#" aria-current="page"
-                      class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">Home</a>
-                  <a href="#"
-                      class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Blog</a>
-                  <a href="#"
-                      class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                  <a href="#"
-                      class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                  <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                  <x-nav-link href="/posts" :active="request()->is('posts')">Blog</x-nav-link>
+                  <x-nav-link href="/about" :active="request()->is('about')">Post</x-nav-link>
+                  <x-nav-link href="/contact" :active="request()->is('contact')">Post</x-nav-link>
               </div>
               <div class="border-t border-white/10 pt-4 pb-3">
                   <div class="flex items-center px-5">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,32 +11,65 @@ Route::get('/about', function () {
     return view('about', ["title" => "About", "name" => "Rizky Pratama"]);
 });
 
-Route::get('/blog', function () {
-    return view('blog', [
+Route::get('/posts', function () {
+    return view('posts', [
         "title" => "Blog", 
-        "blogs" => [
+        "posts" => [
             [
-                "judul" => "Belajar laravel dasar",
-                "penulis" => "Rizky Pratama",
-                "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                \Earum necessitatibus debitis consectetur nostrum nihil vitae atque asperiores 
+                "id" => 1,
+                "slug" => 'belajar-laravel-dasar',
+                "title" => "Belajar laravel dasar",
+                "author" => "Rizky Pratama",
+                "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Earum necessitatibus debitis consectetur nostrum nihil vitae atque asperiores 
                 possimus assumenda ipsam! Explicabo pariatur ab blanditiis architecto tempora 
                 quibusdam aliquam, adipisci consequuntur. Earum fuga quo ducimus id laudantium 
                 ]quibusdam at maxime optio voluptatibus magnam. Quod commodi totam, tempore 
                 suscipit eaque magnam natus."
             ],
             [
-                "judul" => "Tutorial bahasa inggris",
-                "penulis" => "Muhammad Ivan Panteleev",
-                "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                \Earum necessitatibus debitis consectetur nostrum nihil vitae atque asperiores 
-                possimus assumenda ipsam! Explicabo pariatur ab blanditiis architecto tempora 
-                quibusdam aliquam, adipisci consequuntur. Earum fuga quo ducimus id laudantium 
-                ]quibusdam at maxime optio voluptatibus magnam. Quod commodi totam, tempore 
-                suscipit eaque magnam natus."
+                "id" => 2,
+                "slug" => "tutorial-bahasa-inggris",
+                "title" => "Tutorial bahasa inggris",
+                "author" => "Muhammad Ivan Panteleev",
+                "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia rerum iure dolore.
+                Quas corporis pariatur repellat. Vero quasi voluptates optio recusandae ab reprehenderit. Ullam aut sint in
+                hic, eius fuga!"
             ],
         ]
     ]);
+});
+
+Route::get('/posts/{slug}', function($slug) {
+    $posts = [
+            [
+                "id" => 1,
+                "slug" => 'belajar-laravel-dasar',
+                "title" => "Belajar laravel dasar",
+                "author" => "Rizky Pratama",
+                "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                \Earum necessitatibus debitis consectetur nostrum nihil vitae atque asperiores 
+                possimus assumenda ipsam! Explicabo pariatur ab blanditiis architecto tempora 
+                quibusdam aliquam, adipisci consequuntur. Earum fuga quo ducimus id laudantium 
+                ]quibusdam at maxime optio voluptatibus magnam. Quod commodi totam, tempore 
+                suscipit eaque magnam natus."
+            ],
+            [
+                "id" => 2,
+                "slug" => "tutorial-bahasa-inggris",
+                "title" => "Tutorial bahasa inggris",
+                "author" => "Muhammad Ivan Panteleev",
+                "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia rerum iure dolore.
+                Quas corporis pariatur repellat. Vero quasi voluptates optio recusandae ab reprehenderit. Ullam aut sint in
+                hic, eius fuga!"
+            ],
+        ];
+    
+        $post = Arr::first($posts, function($post) use ($slug) {
+            return $post['slug'] == $slug;
+        });
+
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
@@ -45,3 +79,4 @@ Route::get('/contact', function () {
         "instagram" => "@rizkprtama"
     ]);
 });
+
